b3a8b879ee92a915f4b9f7408785bc2535c1d980
Patrick Casey <patrick.casey1@outlook.com>
2023-05-04 18:00:11 -0400

    adding support for stow

---
+++ README.md
@@ -0,0 +1,8 @@
+# .dotfiles
+
+## Contains dotfiles for following programs:
+- alacritty
+- i3
+- neovim
+- tmux
+- zsh
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# TODO pass in command line args to, default to all if none are passed
-if [ ! -x /usr/bin/ansible ]; then
-    sudo apt install software-properties-common
-    sudo apt-add-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible
-fi
-ansible-playbook -i ./local ./main.yaml --ask-become-pass --tags all
