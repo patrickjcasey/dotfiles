0cb5a4f58e01bf38dd6fc564fbb65ee968255843
Patrick Casey <patrick.casey1@outlook.com>
2024-05-17 12:08:42 -0400

    added <leader>sb to use telescope to search buffers

--- nvim/.config/nvim/lua/pcasey/plugins/telescope.lua
+++ nvim/.config/nvim/lua/pcasey/plugins/telescope.lua
@@ -16,7 +16,7 @@ return {
                 mappings = {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-                        ["<C-j>"] = actions.move_selection_next, -- move to next result
+                        ["<C-j>"] = actions.move_selection_next,     -- move to next result
                         ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
                     },
                 },
@@ -32,5 +32,6 @@ return {
         vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
         vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
     end,
 }
