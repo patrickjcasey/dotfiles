5a083e54eaf0aa81f76e2009ac173fcd687ec757
Patrick Casey <patrick.casey1@outlook.com>
2025-03-01 19:53:23 -0500

    made ctrl-u and ctrl-d better (w/ zz added)

--- dotfiles/nvim/lua/pcasey/core/keymaps.lua
+++ dotfiles/nvim/lua/pcasey/core/keymaps.lua
@@ -7,3 +7,7 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })   -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })    -- make split windows equal width & height
+
+-- better ctrl u/d
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving up half a page" })
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half a page" })
