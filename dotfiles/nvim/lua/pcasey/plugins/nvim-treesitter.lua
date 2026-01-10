d90301d4209009f5d07a1434f9529a9e35ce705f
Patrick Casey <trick@Mac.lan>
2026-01-10 16:57:15 -0500

    update nvim-treesitter

--- dotfiles/nvim/lua/pcasey/plugins/nvim-treesitter.lua
+++ dotfiles/nvim/lua/pcasey/plugins/nvim-treesitter.lua
@@ -4,19 +4,7 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         build = ":TSUpdate",
         config = function()
-            -- import nvim-treesitter plugin
-            local treesitter = require("nvim-treesitter.configs")
-
-            -- configure treesitter
-            treesitter.setup({ -- enable syntax highlighting
-                highlight = {
-                    enable = true,
-                },
-                -- enable indentation
-                indent = { enable = true },
-                -- enable autotagging (w/ nvim-ts-autotag plugin)
-                autotag = { enable = true },
-                -- ensure these language parsers are installed
+            require("nvim-treesitter").setup({
                 ensure_installed = {
                     "python",
                     "c",
@@ -33,14 +21,11 @@ return {
                     "toml",
                     "make",
                     "fish",
-                    "make",
                     "css",
                     "asm",
                     "javascript",
                     "typescript",
                 },
-                -- auto install above language parsers
-                auto_install = true,
             })
         end,
     },
